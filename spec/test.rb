32500719b97b8c754a3bdf5e6cb5a69f167ff373
simon <simojenki@gmail.com>
2011-10-28 08:32:48 +0100

    first cut, not too many tests, but seems to work ok

---
+++ spec/test.rb
@@ -0,0 +1,41 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
+require 'cheeky-dreams'
+
+require 'rainbow'
+
+include CheekyDreams
+
+light = Light.new(ansi_driver, 10)
+light.on
+
+puts "colours"
+[:red, :green, :blue, CheekyDreams::rgb(155, 155, 155)].each do |colour|
+  light.go colour 
+  sleep 0.5
+end
+
+puts "cycle"
+light.go(cycle(CheekyDreams::COLOURS.keys, 2))
+sleep 5
+
+puts "fade"
+light.go(fade(:green, :red, 2))
+sleep 3
+light.go(fade(:red, :green, 2))
+sleep 3
+
+puts "fade from current to somewhere"
+
+puts "colour on block"
+cycle = [:blue, :purple].cycle
+light.go(func(2) { cycle.next })
+sleep 3
+
+puts "fake cpu cycles"
+green = 100
+light.go(func(2) { [0, green+=20, 0] })
+sleep 3
+
+puts "done"
