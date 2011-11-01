e02f8073b3a7e5c71c701012f31a0457c2d31714
simon <simojenki@gmail.com>
2011-11-01 23:04:07 +0000

    fixing test.rb

--- spec/test.rb
+++ spec/test.rb
@@ -10,7 +10,7 @@ include CheekyDreams
 light = Light.new(ansi_driver)
 
 puts "throbbing"
-light.go(throb(1, 127, 128))
+light.throb(1, 127, 128)
 sleep 10
 
 puts "is it red"
@@ -24,31 +24,31 @@ puts "colours"
 end
 
 puts "cycle"
-light.go(cycle(CheekyDreams::COLOURS.keys, 2))
+light.cycle(CheekyDreams::COLOURS.keys, 2)
 sleep 5
 
 puts "fade"
-light.go(fade(:green, :red, 20, 2))
+light.fade(:green, :red, 20, 2)
 sleep 3
-light.go(fade(:red, :green, 20, 2))
+light.fade(:red, :green, 20, 2)
 sleep 3
 
 puts "fade from current to somewhere"
 light.go :red
 sleep 2
-light.go fade_to :green, 20, 2
+light.fade_to :green, 20, 2
 sleep 2
-light.go fade_to :red, 20, 2
+light.fade_to :red, 20, 2
 sleep 2
 
 puts "colour on block"
 cycle = [:blue, :purple].cycle
-light.go(func(2) { cycle.next })
+light.func(2) { cycle.next }
 sleep 3
 
 puts "fake cpu cycles"
 green = 100
-light.go(func(2) { [0, green+=20, 0] })
+light.func(2) { [0, green+=20, 0] }
 sleep 3
 
 puts "done"
