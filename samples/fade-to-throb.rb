b4d321efde2734ceb6f773bc8927058a47d430c4
simon <simojenki@gmail.com>
2011-11-17 19:40:55 +0000

    cleaning up some auditing stuff, add samples

---
+++ samples/fade-to-throb.rb
@@ -0,0 +1,13 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'cheeky-dreams'
+
+include CheekyDreams
+
+light = Light.new ansi_driver
+light.auditor = forwarding(:error => suppress_duplicates(stdio_audit))
+light.go crazy
+sleep 2
+light.go fade_to :red
+sleep 100
+
